5a24036cf86b9bf4f32564bb0374c98d54d501a0
Jonathan Jimson <orbitron.hd.official@gmail.com>
2025-12-30 08:56:18 +0530

    Initial test screen

---
+++ app/lib/dose.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dose extends StatelessWidget{
+  const Dose({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dose Test",
+        ),
+      ),
+    );
+  }
+}
